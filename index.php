314cb1bb9994b7c03df036f6146228176e28fcf3
Sourav Kumar Tah <souravtah@gmail.com>
2021-07-31 21:28:07 +0530

    This is a sample factorial program in PHP Version 1

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php  
+$n = 12;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+    $f = $f * $i;  
+}  
+echo "$n! = $f";  
+?> 
+ 
+</body>
+</html>
+<!-- This is a comment -->
